1c6326a0e2b3b409e14aaceb77c32556206f434e
David VALDES <davidvaldes54@gmail.com>
2021-03-03 16:47:10 +0100

    clone

--- src/components/HelloWorldA.vue
+++ src/components/HelloWorldA.vue
@@ -3,9 +3,9 @@
     <p>Bonjour 1</p>
     <p>{{ $route.params.id }}</p>
     <ol>
-        <li v-for="(todo, item) in todos"
+        <li v-for="todo in todos"
         :key="todo.id">
-        {{ todo.content }}<input type="submit" value="Suprimer" @click="suprimerListe(item)">
+        {{ todo.content }}<input type="submit" value="Suprimer" @click="DELETED_TODO(todo.id)"><input type="submit" value="Modifier" @click="EDIT_TODO(todo.id)">
         </li>
     </ol>
     <input v-model="message" value="message">
@@ -24,9 +24,18 @@ export default {
         ...mapState(['todos']),
     },
 
+    props: ['message'],
+
     methods: {
         ajouterListe() {
             this.$store.dispatch('ADD_TODO',this.message)
+        },
+        DELETED_TODO(id) {
+            this.$store.dispatch('DELETE_TODO', id)
+        },
+
+        EDIT_TODO(id) {
+            this.$store.dispatch('EDIT_TODO', id)
         }
     },
 
--- src/store/index.js
+++ src/store/index.js
@@ -19,6 +19,18 @@ export default new Vuex.Store({
 		SET_TODOS(state, todos) {
 			state.todos = todos;
 		},
+
+		DELETE_TODO(state, id) {
+			const index = state.todos.findIndex(message => message.id === id);
+			state.todos.splice(index, 1)
+		},
+
+		EDIT_TODO(state, id) {
+			const todo = state.todos.find(todo => todo.id === id);
+			console.log(todo.content)
+			let newContent
+			todo.content = newContent
+		}
 	},
 
 	actions: {
@@ -30,10 +42,20 @@ export default new Vuex.Store({
 		async ADD_TODO(store, message) {
 			let response = await Axios.post('http://localhost:3000/api/todos/', {
 				content : message
-			});
+			}); store.commit('ajouterListe', response.data);
+		},
 
-			store.commit('ajouterListe', response.data);
+		async DELETE_TODO(store, id) {
+			await Axios.delete(`http://localhost:3000/api/todo/${id}`);
+			store.commit('DELETE_TODO', id);
+		},
+
+		async EDIT_TODO(store, id, newContent) {
+			let response = await Axios.put(`http://localhost:3000/api/todo/put/${id}`,{
+				content : newContent
+			});
+			store.commit('EDIT_TODO', id, response.data)
 		}
-	}
+	},
 })
 
